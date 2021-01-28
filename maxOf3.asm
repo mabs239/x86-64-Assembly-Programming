21f6b73dc929d5482b91be6f2a3818df01c0f541
Abu Bakar <mabs239@users.noreply.github.com>
2021-01-28 19:19:57 +0500

    Update maxOf3.asm

--- maxOf3.asm
+++ maxOf3.asm
@@ -7,33 +7,31 @@ extern printf
 
 section .data
     fmt     db          "Max is: %d",0  
-                        ;llu=long-long-unsignd integer (64 bit)
     num1    dq  17
     num2    dq  14
     num3    dq  5                        
 section .text
 
 main:
-    mov rbp, rsp; for correct debugging
-    push rbp
-    mov rbp, rsp
-    sub rsp, 32
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 32
     
-    mov rcx, [num1]
-    mov rbx, [num2]
-    mov rax, [num3]
+    mov     rcx, [num1]
+    mov     rbx, [num2]
+    mov     rax, [num3]
     
-    cmp rcx, rax
-    jg label1
-    mov rcx, rax
+    cmp     rcx, rax
+    jg      label1
+    mov     rcx, rax
 label1:
-    cmp rcx, rbx
-    jg label2
-    mov rcx, rbx
+    cmp     rcx, rbx
+    jg      label2
+    mov     rcx, rbx
 label2:   
-    mov rdx, rcx;
-    lea rcx, [fmt]
-    call printf
+    mov     rdx, rcx;
+    lea     rcx, [fmt]
+    call    printf
     
     leave
     ret  
