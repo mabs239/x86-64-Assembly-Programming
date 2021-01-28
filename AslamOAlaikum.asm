16d67f2f2499318665e3ef036978fa79557b6c1a
Abu Bakar <mabs239@users.noreply.github.com>
2021-01-28 23:44:21 +0500

    Update AslamOAlaikum.asm

--- AslamOAlaikum.asm
+++ AslamOAlaikum.asm
@@ -3,18 +3,20 @@
 ;of Allah be upon our beloved 
 ;Prophet Muhammad!
 
-global main	
-extern printf
-section .data	
-	  msg1	db	"Aslam O Alaikum!",0	
-section .bss	
-section .text	
+	global 		main	
+	extern 		printf
+	section 	.data	
+msg1	db		"Aslam O Alaikum!",0	
+	section 	.bss	
+	section 	.text	
 main:
-    push	rbp
-    mov		rbp,rsp
-    sub		rsp, 32    
-    mov		rcx, msg1
-    call	printf	
-    mov		rsp,rbp
-    pop		rbp
-    ret
+    	push	rbp
+    	mov	rbp,rsp
+    	sub	rsp, 32    
+    	
+	mov	rcx, msg1
+    	call	printf	
+    	
+	mov	rsp,rbp
+    	pop	rbp
+    	ret
