f8822dffda9b5ac2de6dcc3a2d85027a705703ee
Abu Bakar <mabs239@users.noreply.github.com>
2021-01-28 12:53:02 +0500

    Update Factorial.asm

--- Factorial.asm
+++ Factorial.asm
@@ -2,17 +2,19 @@ global main
 global factorial
 extern printf
 segment .data
-    fmt db "Factorial is: %llu",0;  long long unsignd integer
+    fmt     db          "Factorial is: %llu",0  
+                        ;llu=long-long-unsignd integer (64 bit)
 segment .text
 main:
     push    rbp
     mov     rbp, rsp
     sub     rsp, 32
-    mov     rcx, 20; Place the number in RCX
-    call    factorial; Returns value in RAX
-    lea     rcx, [fmt]; "lea rcx, offset fmt" in masm
-    mov     rdx, rax; as returned from factorial
-    call    printf; takes two parameters in rcx and rdx
+    
+    mov     rcx, 20     ;RCX is input to FACTORIAL function
+    call    factorial   ;Returns value in RAX
+    lea     rcx, [fmt]  ;"lea rcx, offset fmt" in masm
+    mov     rdx, rax    ;RAX is the returned FACTORIAL
+    call    printf      ;takes two parameters in rcx and rdx
     leave
     ret
 
@@ -20,11 +22,11 @@ factorial:
     push    rbp
     mov     rbp, rsp
     sub     rsp, 32
-
+    ; Calculate factorial
     mov     rax, 1
 label1:
-    mul rcx; rax = rcx * rax
-    loop label1
+    mul     rcx         ;rax = rcx * rax
+    loop    label1
     leave
     ret    
     
