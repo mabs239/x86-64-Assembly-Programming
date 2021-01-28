8e3ded1f53f4c26aedcbeaa4147f494a0fe8fc96
Abu Bakar <mabs239@users.noreply.github.com>
2021-01-28 15:53:45 +0500

    Create RecursiveFactorial.asm

---
+++ RecursiveFactorial.asm
@@ -0,0 +1,46 @@
+;All praise be to Almighty Allah 
+;and countless blessings of Allah
+;be upon his beloved Prophet Muhammad
+
+global main
+global factorial
+extern printf
+segment .data
+    fmt     db          "Factorial is: %llu",0  
+                        ;llu=long-long-unsignd integer (64 bit)
+segment .text
+main:
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 32
+    
+    mov     rcx, 3      ;RCX is input to FACTORIAL function
+    call    factorial   ;Returns value in RAX
+    lea     rcx, [fmt]  ;"lea rcx, offset fmt" in masm
+    mov     rdx, rax    ;RAX is the returned FACTORIAL
+    call    printf      ;takes two parameters in rcx and rdx
+    leave
+    ret
+
+factorial:
+    ;input 'n' in rcx
+    ;output factorial(n) in rax 
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 32
+        
+        cmp     rcx, 1; n == 1?
+        jnz recursion;If n>1, recursive call
+        mov rax, 1; factorial(1) = 1
+        jmp factend
+recursion:
+        push rcx; save n
+        dec rcx; n = n-1
+        call factorial; factorial(n-1)
+        pop rcx; restore n
+        mul rcx; n*factorial(n-1)
+
+factend:
+    leave
+            ret  
+    
