9622993a23f63e957893480c6053ba8e1ee612fe
Abu Bakar <mabs239@users.noreply.github.com>
2021-01-31 22:45:16 +0500

    Update BinaryOut.asm

--- BinaryOut.asm
+++ BinaryOut.asm
@@ -3,41 +3,40 @@
 ;of Allah be upon our beloved 
 ;Prophet Muhammad!
 
-global main	
-extern putchar
-section .data	
-section .bss	
-section .text	
+global      main	
+extern      putchar
+section     .data	
+section     .bss	
+section     .text	
 main:
-    push	rbp
-    mov	rbp,rsp
-    sub	rsp, 32    
-   
-
+    push    rbp
+    mov	    rbp,rsp
+    sub	    rsp, 32    
     leave
     ret
+
 printReg:    
     push	rbp
     mov		rbp,rsp
     sub		rsp, 32   
-    mov r13, 64; loopcount
-    mov r12, rcx
+    mov     r13, 64     ;loopcount
+    mov     r12, rcx
 
 TOP:
-    RCl r12,1 ;C. F c bit rotated out
-    JNC NEXT ;O bit
+    RCl     r12,1       ;bit rotated out
+    JNC     NEXT        ;O bit
 
-    mov rcx, '1'
-    call putchar
-    jmp SKIP
+    mov     rcx, '1'
+    call    putchar
+    jmp     SKIP
 NEXT:
-    mov rcx, '0'
-    call putchar
+    mov     rcx, '0'
+    call    putchar
 SKIP:    
-    dec r13
-    jnz TOP
-    mov rcx, 10; newline
-    call putchar
+    dec     r13
+    jnz     TOP
+    mov     rcx, 10;    newline
+    call    putchar
     leave
     ret
     
